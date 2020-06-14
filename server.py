9133982789462f580cbe57f545dad09006fa7a77
parthtyagi27 <parthtyagi27@users.noreply.github.com>
2020-06-14 16:21:40 -0500

    Added initial html files and server code

--- server.py
+++ server.py
@@ -2,6 +2,9 @@ from flask import Flask, request, render_template, jsonify, redirect, url_for
 
 app = Flask(__name__)
 
+@app.route('/')
+def main():
+    return render_template('index.html')
 
 if (__name__ == '__main__'):
     # sys.stdout = sys.stderr = open('server-log.txt', 'wt')
