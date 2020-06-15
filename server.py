d33d8b9739fd92eef8ceb67b6d65b98507ec1260
parthtyagi27 <parthtyagi27@users.noreply.github.com>
2020-06-14 19:51:40 -0500

    Added basic file upload functionality

--- server.py
+++ server.py
@@ -1,10 +1,18 @@
 from flask import Flask, request, render_template, jsonify, redirect, url_for
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
     return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    print("Upload endpoint invoked")
+    input_file = request.files['file']
+    # input_file.save(os.path.join(app.root_path, 'serverInput.ics'))
+    return redirect("/")
 
 if (__name__ == '__main__'):
     # sys.stdout = sys.stderr = open('server-log.txt', 'wt')
